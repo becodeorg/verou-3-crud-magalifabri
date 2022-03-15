8cd4fcfcb672c856dd5dddd1e3153eec943ffeb3
Magali Fabri <magali_90@live.nl>
2022-03-15 10:42:47 +0100

    Update index and overview to show table of collection

--- index.php
+++ index.php
@@ -1,42 +1,42 @@
 <?php
 
-// Require the correct variable type to be used (no auto-converting)
-declare (strict_types = 1);
+declare(strict_types=1);
 
-// Show errors so we get helpful information
 ini_set('display_errors', '1');
 ini_set('display_startup_errors', '1');
 error_reporting(E_ALL);
 
-// Load you classes
 require_once 'config.php';
 require_once 'classes/DatabaseManager.php';
 require_once 'classes/CardRepository.php';
 
-$databaseManager = new DatabaseManager($config['host'], $config['user'], $config['password'], $config['dbname']);
+// create DatabaseManager
+$databaseManager = new DatabaseManager(
+    $config['host'],
+    $config['user'],
+    $config['password'],
+    $config['dbname']
+);
 $databaseManager->connect();
 
-// This example is about a Pokémon card collection
-// Update the naming if you'd like to work with another collection
+// get cards
 $cardRepository = new CardRepository($databaseManager);
 $cards = $cardRepository->get();
 
-// Get the current action to execute
-// If nothing is specified, it will remain empty (home should be loaded)
+// get action
 $action = $_GET['action'] ?? null;
 
-// Load the relevant action
-// This system will help you to only execute the code you want, instead of all of it (or complex if statements)
 switch ($action) {
     case 'create':
-        $this->create();
+        create();
         break;
+
     default:
-        $this->overview();
+        overview($cards);
         break;
 }
 
-function overview()
+function overview($cards)
 {
     // Load your view
     // Tip: you can load this dynamically and based on a variable, if you want to load another view
--- overview.php
+++ overview.php
@@ -1,21 +1,36 @@
 <!doctype html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Goodcard - track your collection of Pokémon cards</title>
 </head>
+
 <body>
 
-<h1>Goodcard - track your collection of Pokémon cards</h1>
+    <h1>Goodcard - track your collection of Pokémon cards</h1>
 
-<ul>
-    <?php foreach ($cards as $card) : ?>
-        <li><?= $card['name'] ?></li>
-    <?php endforeach; ?>
-</ul>
+    <table>
+        <tr>
+            <th>#</th>
+            <th>name</th>
+            <th colspan="2">type(s)</th>
+            <th>description</th>
+        </tr>
+        <?php foreach ($cards as $card) : ?>
+            <tr>
+                <td><?= $card['number'] ?></td>
+                <td><?= $card['name'] ?></td>
+                <td><?= $card['type1'] ?></td>
+                <td><?= $card['type2'] ?></td>
+                <td><?= $card['description'] ?></td>
+            </tr>
+        <?php endforeach; ?>
+
+    </table>
 
 </body>
+
 </html>
