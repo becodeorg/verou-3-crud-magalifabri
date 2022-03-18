5f8f3e3681f8a9e4eb19eb28b698c0fa19d4b332
Magali Fabri <magali_90@live.nl>
2022-03-18 08:14:30 +0100

    Add heroku db connection

--- Classes/DatabaseManager.php
+++ Classes/DatabaseManager.php
@@ -20,8 +20,22 @@ class DatabaseManager
     public function connect(): void
     {
         try {
-            $DSN = "mysql:host={$this->host};dbname={$this->dbname}";
-            $this->connection = new PDO($DSN, $this->user, $this->password);
+            if (!empty(getenv("DATABASE_URL"))) {
+                $dbParams = parse_url(getenv("DATABASE_URL"));
+
+                $this->connection = new PDO("pgsql:" . sprintf(
+                    "host=%s;port=%s;user=%s;password=%s;dbname=%s",
+                    $dbParams["host"],
+                    $dbParams["port"],
+                    $dbParams["user"],
+                    $dbParams["pass"],
+                    ltrim($dbParams["path"], "/")
+                ));
+            } else {
+                $DSN = "mysql:host={$this->host};dbname={$this->dbname}";
+                $this->connection = new PDO($DSN, $this->user, $this->password);
+            }
+
             $this->connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         } catch (PDOException $e) {
             echo "Error: " . $e->getMessage();
